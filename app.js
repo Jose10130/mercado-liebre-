094825be417432403e56a23ac5c7a0024786251d
jose10130 <jose10130.jcd@gmail.com>
2024-02-01 01:49:00 -0300

    trate de hacer lo de table y pe pero no puedo (estoy cansado jefe)

--- app.js
+++ app.js
@@ -7,11 +7,18 @@ app.use(express.static('public'))
 
 app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, "./views/home.html"))
-})
-
-app.get('/home', (req, res)=>{
+    app.get('/home', (req, res)=>{
     res.redirect('/')
 })
+});
+app.get('/registro', (req, res)=>{
+    res.sendFile(path.join(__dirname, "./views/register.html"))
+
+app.get('/register', (req, res)=>{
+    res.redirect('/')
+})
+})
+
 
 
 
